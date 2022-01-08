88f4760d955e836a571318265921694c264e3307
RiyaRaturi <97171339+RiyaRaturi@users.noreply.github.com>
2022-01-08 18:40:40 +0530

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,26 @@
+#import the modules!
+import random
+import string
+
+print('hello,welcome to the password generator!')
+
+#input the length of password
+length=int(input('\Enter the length of password:'))
+
+#define data
+lower=string.ascii_lowercase
+upper=string.ascii_uppercase
+num=string.digits
+symbols=string.punctuation
+
+#combine the data
+all=lower+upper+num+symbols
+
+#use random
+temp=random.sample(all,length)
+
+#create the password
+password="".join(temp)
+
+#print the password
+print(password)
